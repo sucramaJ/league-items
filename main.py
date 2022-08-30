cdc54fa32148d8a220b977406a09d088b08bedb4
sucramaJ <jamarcus.snyders@gmail.com>
2022-08-30 11:05:18 +0200

    Included champion example

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from utils import credentials
+import cassiopeia as cass
+import os
 
 credentials.set_environ()
+
+champions = cass.Champions(region="NA")
+for champion in champions:
+    print(champion.name, champion.id)
