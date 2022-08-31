92e859cf13818074db65e7413e61a79278dda343
sucramaJ <jamarcus.snyders@gmail.com>
2022-08-31 13:08:19 +0200

    Created CLI print item function

--- items.py
+++ items.py
@@ -1,11 +1,18 @@
 import cassiopeia as cass
 from PIL import Image
+import pprint
+
+def print_item(item):
+    print(f"\n{item.name}:")
+    for key, val in item.to_dict().items():
+        print(f"{key}: {val}")
+
 
 items = cass.get_items(region="NA")
 null_magic_mantle = items[110]
 dagger = cass.Item(name="Dagger", region="NA")
 
-print(items[110].gold.to_dict())
-print(dagger.gold.to_dict())
-print(dagger.stats.to_dict())
-dagger.image.image.show()
+#print(items[110].gold.to_dict())
+print_item(dagger)
+#print(dagger.stats.to_dict())
+#dagger.image.image.show()
