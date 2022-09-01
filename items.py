d17c48a072072c24fcadee581521d25b595a5e24
sucramaJ <jamarcus.snyders@gmail.com>
2022-09-01 06:44:19 +0200

    Item scraping functions

--- items.py
+++ items.py
@@ -2,17 +2,34 @@ import cassiopeia as cass
 from PIL import Image
 import pprint
 
-def print_item(item):
-    print(f"\n{item.name}:")
-    for key, val in item.to_dict().items():
-        print(f"{key}: {val}")
+def print_item(item_dict):
+    print(f"\n{item_dict['name']}:")
+    for key, val in item_dict.items():
+        print(f"{key}: {val}\n")
+
+def clean_item(item_dict):
+    clean_features = ['name','stats', 'plaintext', 'buildsInto', 'sanitizedDescription','gold','id','image']
+    clean_item = {key: value for key, value in item_dict.items() if key in clean_features}
+    return clean_item
+
+def get_item_data():
+    items_dict = {}
+    items = cass.get_items(region="NA")
+    item = items[110]
+    item = item.to_dict()
+    print_item(item)
+    print('-------------')
+    cleaned_item = clean_item(item)
+    print_item(cleaned_item)
 
 
-items = cass.get_items(region="NA")
-null_magic_mantle = items[110]
-dagger = cass.Item(name="Dagger", region="NA")
+if __name__ == "__main__":
+    #items = cass.get_items(region="NA")
+    #null_magic_mantle = items[110]
+    #dagger = cass.Item(name="Dagger", region="NA")
 
-#print(items[110].gold.to_dict())
-print_item(dagger)
-#print(dagger.stats.to_dict())
-#dagger.image.image.show()
+    #print(items[110].gold.to_dict())
+    #print_item(dagger)
+    #print(dagger.stats.to_dict())
+    #dagger.image.image.show()
+    get_item_data()
