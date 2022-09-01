4eb8704ee6fa9bd3d442adb5a67e02f468f820af
sucramaJ <jamarcus.snyders@gmail.com>
2022-09-01 15:05:17 +0200

    Finished detag_text function. Strips description of each item of any html tags.

--- items.py
+++ items.py
@@ -1,25 +1,45 @@
 import cassiopeia as cass
 from PIL import Image
+import re
 import pprint
 
 def print_item(item_dict):
+    '''
+    Function for printing an item in the terminal.
+    '''
     print(f"\n{item_dict['name']}:")
     for key, val in item_dict.items():
         print(f"{key}: {val}\n")
 
-def clean_item(item_dict):
-    clean_features = ['name','stats', 'plaintext', 'buildsInto', 'sanitizedDescription','gold','id','image']
-    clean_item = {key: value for key, value in item_dict.items() if key in clean_features}
-    return clean_item
+def detag_text(text):
+    '''
+    Remove html tags from item descriptions.
+    '''
+    clean_re = '<.*?>'
+    text = text.replace('<br>',', ')
+    clean_text = re.sub(clean_re, '', text)
+    print(f"{clean_text} clean text")
+    return clean_text
+
+def strip_item(item_dict):
+    '''
+    Strips item of any features we do not care about
+    '''
+    strip_features = ['name','stats', 'plaintext', 'buildsInto', 'sanitizedDescription','gold','id','image']
+    stripped_item = {key: value for key, value in item_dict.items() if key in strip_features}
+    stripped_item['sanitizedDescription'] = detag_text(stripped_item['sanitizedDescription'])
+    return stripped_item
 
 def get_item_data():
+    '''
+    Function to download item data and store it in a JSON file.
+    '''
     items_dict = {}
     items = cass.get_items(region="NA")
     item = items[110]
     item = item.to_dict()
-    print_item(item)
     print('-------------')
-    cleaned_item = clean_item(item)
+    cleaned_item = strip_item(item)
     print_item(cleaned_item)
 
 
